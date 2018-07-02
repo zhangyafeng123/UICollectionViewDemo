3f9c1c49344b1317842830234809e37515fea037
zhangyafeng123 <1240334911@qq.com>
2018-07-02 20:21:40 +0800

    Initial Commit

---
+++ UICollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewDemo
+//
+//  Created by linjianguo on 2018/7/2.
+//  Copyright © 2018年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewDemo
+//
+//  Created by linjianguo on 2018/7/2.
+//  Copyright © 2018年 com.justsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
